9f7b3a9e06d38ac05b82cdb07f16a5e632a48f1d
jluthi-gcu <113372169+jluthi-gcu@users.noreply.github.com>
2022-12-01 22:52:56 -0700

    Delete Milestone 3 directory

--- Milestone 3/doc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"StoreFront","l":"initializeStore()"},{"p":"<Unnamed>","c":"StoreFront","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"StoreFront","l":"StoreFront()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"StoreFront","l":"unitTest()"}];updateSearchResults();
